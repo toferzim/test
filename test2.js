5ded2907ecbc686bfdbdfb43742268005b115dc4
Anton <anton.zimin@gmail.com>
2020-06-04 21:29:56 +0300

    Домашнее задание

--- test2.js
+++ test2.js
@@ -1,6 +1,19 @@
-let switcher = null;
-if (switcher) {
-    console.log("Working");
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате:", "ГГГГ-ММ-ДД");
+let expenses = {
+    answer1: "",
+    answer2: ""
 }
-console.log(typeof(switcher));
-alert('Hello world!')
+expenses.answer1 = prompt("Введите обязательную статью расходов в этом месяце") + " : " + prompt("Во сколько обойдется?");
+expenses.answer2 = prompt("Введите обязательную статью расходов в этом месяце") + " : " + prompt("Во сколько обойдется?");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expensesFromObj: expenses,
+    optionalExpenses: "",
+    income: {},
+    savings: false
+
+}
+alert (money/30);
